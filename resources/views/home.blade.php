3658cf38ceb87307daf622db406448ba9fc637f6
danielmegale <danim1@live.it>
2023-08-23 16:17:52 +0200

    added train_db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @Vite('resources\js\app.js')
-</head>
-
-<body>
-
-</body>
-
-</html>
+@section('main')
+    <h1>Train</h1>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Train</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link @if (Route::is('home')) active @endif" aria-current="page"
+                        href="{{ route('home') }}">Home</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
